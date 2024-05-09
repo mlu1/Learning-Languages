97b9c520c79ef752a28ad81ca42395b41e1271b5
mlu@1 <mthandemeluleki@yahoo.com>
2024-05-10 01:37:49 +0800

    Fix bug

--- Angular/todo-list/src/app/app.component.ts
+++ Angular/todo-list/src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule  } from '@angular/common';
+import { publicDecrypt } from 'crypto';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'todo-list';
-  tasks =["Visit Ann","Go to the gym","Wash the dishes","Shop for the party"]
+  tasks = [
+    "Visit Ann",
+    "Call Dad",
+    "Go to the gym",
+    "Wash the dishes",
+    "Shop for the party"]
+
+  add (newTask:string){
+    this.tasks.push(newTask)
+  }
+
+  remove(existingTask:string){
+   var userConfirmed =  confirm('Are you sure, "${existingTask}"');
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task => task != existingTask); 
+    }
+  }
+
+  markAsDone(task:string){
+    alert('The task:"' + task + '" is done')
+  }
+}
+
+class Task {
+  constructor(public title :string){
+  }
 }
