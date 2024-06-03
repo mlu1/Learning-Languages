966455904e6d0e45968c04fec1fef6b9d96a709e
mlu@1 <mthandemeluleki@yahoo.com>
2024-06-03 18:17:25 +0800

    OOP lesson

---
+++ python/keyboard.py
@@ -0,0 +1,8 @@
+from item import Item
+class Keyboard(Item):
+     pay_rate = 0.7
+     def __init__(self,name:str,price:float,quantity=0,broken_phones=0):
+        super().__init__(
+            name,price,quantity
+        )  
+        
