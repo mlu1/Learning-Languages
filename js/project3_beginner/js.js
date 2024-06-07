9304310ba87e0db596b32b50d85fac23b53c5900
mlu@1 <mthandemeluleki@yahoo.com>
2024-06-07 12:29:41 +0800

    game based html

---
+++ js/project3_beginner/js.js
@@ -0,0 +1,30 @@
+/*var canvas = document.getElementById('canvasSpace');
+var ctx = canvas.getContext("2d")
+ctx.fillText("helloworld",10,150);*/
+
+var canvas =document.createElement("canvas")
+var context = canvas.getContext("2d")
+canvas.height= 400
+canvas.width = 600
+mainImage = new Image();
+mainImage.ready =false;
+mainImage.onload = checkReady;
+mainImage.src = "pac.png"
+
+function checkReady(){
+    this.ready = true;
+    playgame();
+}
+
+function playgame(){
+    render();
+}
+
+function render(){
+    context.fillStyle = "black";
+    context.fillRect(0,0,canvas.width,canvas.height)
+}
+
+
+document.body.appendChild(canvas);
+context.fillText("helloworld",10,150)
