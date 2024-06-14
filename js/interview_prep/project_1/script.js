b122aef5268ca0eac31833f38494ff45c138311f
mlu@1 <mthandemeluleki@yahoo.com>
2024-06-14 19:27:58 +0800

    more preparations

--- js/interview_prep/project_1/script.js
+++ js/interview_prep/project_1/script.js
@@ -1,5 +1,5 @@
 //What is function decalration?
-function suare(num){
+function square(num){
     return num*num;
 }
 
@@ -15,7 +15,7 @@ const square_values = function (num) {
 function displaySquare(fn){
     console.log("Square is " + fn(5));
 }
-displaySquare(square_values)
+//displaySquare(square_values)
 
 /*
 Q4  What is an IIFE (Immediately Invoked Function Expression)
@@ -33,14 +33,55 @@ Q4  What is an IIFE (Immediately Invoked Function Expression)
     })(2);
 })(1);
 /*
-Becasue of closure
+Becasue of closure the inner function is able to
+access other variables that are not in this function. 
+See example below
 */
+
+
+var n1 = 20,
+    n2 =3,
+    names = 'Mluleki';
+
+
+function multiply(){
+    return n1 *n2;
+}
+
+console.log(multiply());
+
+function getScore(){
+    var n1 = 2,
+    n2 = 3
+
+    function add(){
+        return names + " scored " + (n1 + n2);   
+    }
+    return add();
+}
+
+console.log(getScore()); //returns 5
+
 
 /*
   The primary reason to use an IIFE is to obtain data privacy because any variables
   declared within the IIFE cannot be accessed by the outside world. 
   i.e, If you try to access variables from the IIFE then it throws an error as below,
 */
+
+/*Q7
+
+*/
+
+for(var i = 0;i<5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },i* 1000);
+}
+
+
+
+
 
 
 
