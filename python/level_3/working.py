03e97a12347d9dea308976be07d5c2d5f7a3c235
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-12 22:14:53 +0800

    level 3

---
+++ python/level_3/working.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+inventory = {
+    1:{
+        "name":"Milk",
+        "price":3.99,
+        "brand":"Regular"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id:int):
+    return inventory[item_id]
+
+@app.get("/")
+def home():
+    return {"Data":"Testing"}
+
+@app.get("/about")
+def about():
+    return {"Data":"About"} 
