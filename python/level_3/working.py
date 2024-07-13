34f8a987b982c2045bd2cff5f60849ea424cfe74
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-13 13:05:56 +0800

    Data levels

--- python/level_3/working.py
+++ python/level_3/working.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -11,8 +12,16 @@ inventory = {
 }
 
 @app.get("/get-item/{item_id}")
-def get_item(item_id:int):
+def get_item(item_id:int = Path(description="The ID of the item that you would like",gt=0,lt=2)):
     return inventory[item_id]
+
+##A query parameter
+@app.get("/get-by-name")
+def get_item(*,name:Optional[str] = None,test:int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data":"Not found"}
 
 @app.get("/")
 def home():
