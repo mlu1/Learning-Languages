7a562fb9dd95f1a478ff5cb3818a6e85ce681c8d
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-16 15:02:05 +0800

    status

---
+++ python/100Days/Day1/main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("A fnction is declared like this:")
+print("Another string!")
+
+#A new line
+print("Hello World! \n Hello Again \n Hello last timme")
+#string concatenation
+print("Hello" + "Mluleki")
+# with double quotes to get a space
+print("Hello" +" "+"Mluleki")
+
+#######An Exercise########
+print("Day 1 string manipulation")
+print("String Concatenation is done with the + sign")
+print('eg. print("Hello + World")')
+print("New lines can be created with a backslash")
