b44262391e7b16ce3f5ae6e8eb8e3a6fb1680573
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-17 22:55:32 +0800

    new day

---
+++ python/100Days/Day2/day_2.py
@@ -0,0 +1,25 @@
+#Data Types
+
+#String
+print("Hello"[0])
+print("Hello"[1])
+
+print("hello" "world")
+
+#Integer
+print(123 +245)
+
+#Float
+3.14159
+
+#Boolean
+a = True
+b = False
+
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has "+new_num_char+" characters")
+
+a = float(123)
+print(type(a))
+print(70 +float("100.5"))
