b05b72794e4cd23d48bacd2357729aeebef0a4c8
mlu1 <mthandemeluleki@yahoo.com>
2024-10-28 09:49:57 +0800

    continue lessons

--- js/js_zero_to_hero/variables.js
+++ js/js_zero_to_hero/variables.js
@@ -1,1 +1,20 @@
-var title = "javascript course"
+let title = 'Javascript Course'
+console.log(title)
+var subtitle = 'javaScript Course'
+
+let str  = '  JavaAndScript  '
+console.log(str.length)
+let trimStr = str.trim(0)
+console.log(trimStr)
+console.log('After trim ',trimStr.length)
+console.log(trimStr.toUpperCase())
+console.log(trimStr.toLowerCase())
+console.log(trimStr.slice(0,2))
+
+/**
+ * INTRODUUCTION
+ */
+
+let age = 35
+let fullname = 'Mluleki Mtande'
+console.log(`The age is ${age} and name is ${fullname}`)
