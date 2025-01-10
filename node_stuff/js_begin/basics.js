dcec9b6b27ec3ce0894d7102bcf16f3cce3125d8
mlu1 <mthandemeluleki@yahoo.com>
2025-01-10 11:15:28 +0800

    refresh javascript

---
+++ node_stuff/js_begin/basics.js
@@ -0,0 +1,22 @@
+let v1 = 7;
+let v2  = true;
+const globalv1 = "Im global"
+
+/**
+ * Multi-line Comment
+ */
+
+my_v1 = "helllo";
+const pi = 3.14159;
+
+if (pi<4){
+    console.log('Yes,pi is less than 4')
+}else if(pi===4){
+    console.log('PI is equal to 4')
+}
+else if(pi === 5){
+    console.log('PI is equal to 4')
+}
+else{
+    console.log('No, pi is greater than or equal to')
+}
