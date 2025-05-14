d60847322d2aa7d8d6ae0cbc692e123fd0c6d4fc
mlu1 <mthandemeluleki@yahoo.com>
2025-05-14 22:17:34 +0800

    tuples

--- python/Simple/Lesson3.py
+++ python/Simple/Lesson3.py
@@ -9,3 +9,23 @@ if name1 == name2:
     print("Strings are equal")
 else:
     print("strings are not equal")
+
+'''
+Slice
+'''
+OS= ["Windoes","Linux","Android","iOS"]
+print(OS[0:4])
+print(OS[2:4])
+print(OS[-3:])
+
+for item in OS[0:4]:
+    print(item)
+    
+    
+ '''
+ TUPLES
+ '''
+tups = (200,50)
+print(tups)
+
+
